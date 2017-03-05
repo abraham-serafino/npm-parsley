e5f76cc2517efee8cb72c8d26e1d2feb19e334bd
serafino <abraham.serafino@gmail.com>
2017-03-05 00:57:48 -0600

    Export something that actually works

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var jquery = require('jquery');
 window.$ = jquery;
 require('parsley');
 
-module.exports = $.parsley;
+module.exports = function(selector, options) {
+  $(selector).parsley(options);
+};
